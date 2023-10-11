d132bd4dae1ca15338e65e9663ddc8da90565b52
xx <xx@xx.com>
2023-10-11 09:50:55 +0800

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,9 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Hello from "./components/Hello";
-import LikeButton from "./components/LikeButton";
-import MouseTracker from "./components/MouseTracker";
-import useMousePosition from "./hooks/useMousePosition";
-import useURLLoader from "./hooks/useURLLoader";
+import Button from "./components/Button/button"
 
-type dogDataType = {
-  message: string
-}
-function App() {
-  const position = useMousePosition();
-  const [data, dogLoading] = useURLLoader('https://dog.ceo/api/breeds/image/random', [])
-  const dogData = data as dogDataType
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Hello message="nihao" />
-        <LikeButton />
-        <MouseTracker />
-        {
-          dogLoading ? <h2>loading</h2>
-          : <img src={dogData && dogData.message} alt="" />
-        }
-        <p>
-          hooks: x: {position.x}, y: {position.y}
-        </p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return <>
+    <Button>按钮</Button>
+  </>
 }
 
-export default App;
+export default App
---
+++ src/components/Button/button.tsx
@@ -0,0 +1,57 @@
+import classNames from "classnames";
+
+export enum ButtonSize {
+  Large = "lg",
+  Small = "sm",
+}
+
+export enum ButtonType {
+  Primary = "primary",
+  Default = "default",
+  Danger = "danger",
+  Link = "link",
+}
+
+interface BaseButtonProp {
+  className?: string;
+  disabled?: boolean;
+  size?: ButtonSize;
+  btnType?: ButtonType;
+  children: React.ReactNode;
+  href?: string;
+}
+
+const Button: React.FC<BaseButtonProp> = ({
+  disabled,
+  size,
+  btnType,
+  children,
+  href,
+}) => {
+  const classes = classNames("btn", {
+    [`btn-${btnType}`]: btnType,
+    [`btn-${size}`]: size,
+    disabled: btnType === ButtonType.Link && disabled,
+  });
+
+  if (btnType === ButtonType.Link && href) {
+    return (
+      <a className={classes} href={href}>
+        {children}
+      </a>
+    );
+  }
+
+  return (
+    <button className={classes} disabled={disabled}>
+      {children}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+    disabled: false,
+    btnType: ButtonType.Default
+}
+
+export default Button;
